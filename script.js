f40d9d96ba0d07cf6755eeae6498642348961755
AlexJosu027 <Alexandru.Josu@edu.sait.ca>
2026-03-18 11:50:04 -0600

    7 Primatives

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// 7 primitives in JS
 
+// Null Type
+typeof null === "object";
+
+// Undefined Type
+let unknown;
+
+// Boolean Type
+const isActive = true;
+
+// Number Type
+let number = 5;
+
+// BigInt Type
+const Range = BigInt(0, number);
+
+// String Type
+const string = "Hello World";
+
+// Symbol Type
+const mySymbol1 = Symbol("description");
+const mySymbol2 = Symbol("description");
+console.log(mySymbol1 === mySymbol2);
